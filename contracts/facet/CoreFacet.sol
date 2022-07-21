5d64ef344222fd944f18d3f5ca11ff2c6578a4d5
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2022-07-21 19:32:46 -0400

    battle contracts + circuit

--- contracts/facet/CoreFacet.sol
+++ contracts/facet/CoreFacet.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 import {WithStorage} from "../library/LibStorage.sol";
 import {LibMIMC} from "../library/LibMIMC.sol";
+import {LibTrig} from "../library/LibTrig.sol";
 import {Verifier as InitVerifier} from "../library/InitVerifier.sol";
 import {Verifier as MoveVerifier} from "../library/MoveVerifier.sol";
+import {Verifier as BattleVerifier} from "../library/BattleVerifier.sol";
 import "hardhat/console.sol";
 
 contract CoreFacet is WithStorage {
@@ -46,6 +48,10 @@ contract CoreFacet is WithStorage {
         uint256[2] memory _c,
         uint256[4] memory _input
     ) public notPaused {
+        uint256[] memory addr = new uint256[](1);
+        addr[0] = uint256(uint160(msg.sender));
+        uint256 addressPhase = LibMIMC.mimcSponge(addr, 1, 22, 123)[0] % LibTrig.TWO_PI;
+
         uint256 possibleHashesHash = assembleHash(blockNumLower, blockNumUpper);
         require(_input[0] == possibleHashesHash, "Block number commitment hash mismatch");
         require(_input[1] == gs().saltUpperBound, "Salt upper bound mismatch");
@@ -54,6 +60,7 @@ contract CoreFacet is WithStorage {
         require(gs().playerStates[msg.sender].commitment == 0, "Player already initialized");
 
         gs().playerStates[msg.sender].commitment = _input[3];
+        gs().playerStates[msg.sender].phase = addressPhase;
         emit PlayerUpdated(msg.sender, gs().playerStates[msg.sender].commitment, block.number);
     }
 
@@ -76,5 +83,27 @@ contract CoreFacet is WithStorage {
 
         gs().playerStates[msg.sender].commitment = _input[4];
         emit PlayerUpdated(msg.sender, gs().playerStates[msg.sender].commitment, block.number);
+    }
+
+    function battlePlayer(
+        address player,
+        uint256[2] memory _a,
+        uint256[2][2] memory _b,
+        uint256[2] memory _c,
+        uint256[2] memory _input
+    ) public notPaused {
+        uint256 myCommitment = gs().playerStates[msg.sender].commitment;
+        uint256 yourCommitment = gs().playerStates[player].commitment;
+        int256 myPower = LibTrig.sin(
+            (block.number + gs().playerStates[msg.sender].phase) % LibTrig.TWO_PI
+        );
+        int256 yourPower = LibTrig.sin(
+            (block.number + gs().playerStates[player].phase) % LibTrig.TWO_PI
+        );
+
+        require(_input[0] == myCommitment, "My commitment hash mismatch");
+        require(_input[1] == yourCommitment, "Your commitment hash mismatch");
+        require(BattleVerifier.verifyProof(_a, _b, _c, _input), "Bad proof");
+        require(myPower > yourPower, "My power is not greater than your power");
     }
 }

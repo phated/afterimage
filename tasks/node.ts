97564abef6f6a62c460b348b4a0b0ca4cecf7604
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-09-12 17:21:29 -0700

    feat: Start vite from hardhat after contracts deployed (#20)
    feat: Depend on hardhat-vite plugin
    chore!: Remove `npm start` command in client workspace
    chore: Remove browserlist because they are targets to vite now

--- tasks/node.ts
+++ tasks/node.ts
@@ -1,15 +1,24 @@
 import type { HardhatRuntimeEnvironment, RunSuperFunction } from 'hardhat/types';
 import { subtask } from 'hardhat/config';
 import { TASK_NODE_SERVER_READY } from 'hardhat/builtin-tasks/task-names';
+import { TASK_VITE } from 'hardhat-vite';
 
 subtask(TASK_NODE_SERVER_READY, nodeReady);
 
 async function nodeReady(
-  args: unknown,
+  args: { address: string; port: number },
   hre: HardhatRuntimeEnvironment,
-  runSuper: RunSuperFunction<unknown>
+  runSuper: RunSuperFunction<{ address: string; port: number }>
 ) {
   await runSuper(args);
 
   await hre.run('deploy');
+
+  await hre.run(TASK_VITE, {
+    command: 'serve',
+    // Adding things here will be available in `import.meta.env`
+    env: {
+      RPC_URL: `http://${args.address}:${args.port}`,
+    },
+  });
 }
